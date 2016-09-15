6db9d294b1605d29d3ffc59b0b6020b335cb4209
Kenneth Moy <moykenneth91@gmail.com>
2016-09-15 16:17:38 -0400

    Updated mergesort to get a merge function

--- mergesort.js
+++ mergesort.js
@@ -6,6 +6,40 @@ var secondHalf = wholeArray.slice(wholeArray.length/2)
   return [firstHalf, secondHalf];
 }
 
+function merge(arr1, arr2){
+    var sorted = [];
+     while(arr1.length > 0 && arr2.length > 0) {
+         arr1[0] < arr2[0] ? sorted.push(arr1.shift()) 
+            : sorted.push(arr2.shift());
+     }
+     if(arr1.length > 0) {
+         sorted = sorted.concat(arr1);
+     }
+     if(arr2.length > 0) {
+        sorted = sorted.concat(arr2);
+     }
+     return sorted;
+}
+
+function mergeSort(array) {;
+     if(array.length === 1 || array.length === 0 ) {return array};
+     
+     var first;
+     var second;
+     if(array.length > 2) {
+        var splits =  split(array);
+        first = mergeSort(splits[0]);
+        second = mergeSort(splits[1]);
+     } else {
+         // Since array.length is 2, that means, 2 number values
+         return merge([array[0]], [array[1]]);
+     }
+
+     return merge(first, second);
+}
+
+// Integrated solution
+/*
 function mergeSort(array) {
      if(array.length === 1 || array.length === 0 ) {return array};
 
@@ -34,3 +68,4 @@ function mergeSort(array) {
      }
      return sorted;
 }
+*/
